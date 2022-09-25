c24bd79905906f2b3087ef9473fe42e6465fe45a
vuchvu <akisamegane7100@gmail.com>
2022-09-25 21:13:37 +0900

    add Plugin 'hardlink'

--- plugin/mod.ts
+++ plugin/mod.ts
@@ -1,5 +1,6 @@
 import Symlink from "./symlink.ts";
+import Hardlink from "./hardlink.ts";
 import Shell from "./shell.ts";
 import Repository from "./repository.ts";
 
-export { Repository, Shell, Symlink };
+export { Repository, Shell, Symlink, Hardlink };
