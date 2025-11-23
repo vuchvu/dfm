6b4002dd2180b4773df0d6319667c9fcd583be37
vuchvu <vuchvu@users.noreply.github.com>
2025-11-23 14:15:39 +0900

    can resolve homedir in Windows

--- util/util.ts
+++ util/util.ts
@@ -1,16 +1,19 @@
 import { join, resolve } from "../deps.ts";
 
-const homedir = Deno.env.get("HOME");
+const getHomeDir = () {
+  if(os() === "windows") {
+    return Deno.env.get("USERPROFILE");
+  } else {
+    return Deno.env.get("HOME");
+  }
+}
 
 export function expandTilde(path: string) {
-  if (homedir === undefined) {
-    return path;
-  } else {
-    if (!path) return path;
-    if (path === "~") return homedir;
-    if (path.slice(0, 2) !== "~/") return path;
-    return join(homedir, path.slice(2));
-  }
+  const homedir = getHomeDir();
+  if (!path) return path;
+  if (path === "~") return homedir;
+  if (path.slice(0, 2) !== "~/") return path;
+  return join(homedir, path.slice(2));
 }
 
 // BASEDIR is basedir of dotfiles
